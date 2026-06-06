ab956494d606945a01b970cd99be55480f09bb61
Josiah Raziel S. Lluch <josiahraziel.lluch@g.msuiit.edu.ph>
2026-06-07 04:26:40 +0800

    updated semester card

--- src/components/SemesterCard.tsx
+++ src/components/SemesterCard.tsx
@@ -10,16 +10,30 @@ export const SemesterCard = () => {
     return (
         <Card className="@container/card">
             <CardHeader className="">
-                <CardTitle>Courses</CardTitle>
-                <CardDescription>
-                                        <span className="hidden @[540px]/card:block">
-                                            All enrolled courses during the semester
-                                        </span>
-                    <span className="@[540px]/card:hidden">semester</span>
-                </CardDescription>
+                <div className="flex flex-row gap-4 p-4">
+                    <div className="">
+                        <div>
+                            <CardTitle>Courses</CardTitle>
+                            <CardDescription>
+                        <span className="hidden @[540px]/card:block">
+                            All enrolled courses during the semester
+                        </span>
+                                <span className="@[540px]/card:hidden">semester</span>
+                            </CardDescription>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="text-sm font-medium">
+                            Semester 1
+                        </div>
+                        <div className="text-sm font-medium">
+                            2025-2026
+                        </div>
+                    </div>
+                </div>
             </CardHeader>
             <CardContent>
-                <CourseDataTable data={data as Course}/>
+                <CourseDataTable data={data as Course[]}/>
             </CardContent>
         </Card>
     )
