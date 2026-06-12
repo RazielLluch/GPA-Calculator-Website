fcbd3a9ca8d79873d6532a8c0ba669f9bf023a2a
Josiah Raziel S. Lluch <josiahraziel.lluch@g.msuiit.edu.ph>
2026-06-13 07:01:50 +0800

    added host origins

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,8 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+    allowedDevOrigins: ['192.168.0.104', 'http://192.168.0.104:3000', '192.168.0.104:3000']
+}
+
 export default nextConfig;
